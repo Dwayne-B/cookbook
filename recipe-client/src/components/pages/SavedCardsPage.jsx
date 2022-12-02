d1bb4a3ddbc4a90fc4c2d322cb8a7658972a7258
Dwayne Bynum <dbynum4597@gmail.com>
2022-12-02 01:49:23 -0500

    added update feature

--- recipe-client/src/components/pages/SavedCardsPage.jsx
+++ recipe-client/src/components/pages/SavedCardsPage.jsx
@@ -1,52 +1,105 @@
 import { useState } from 'react';
+import { json } from 'react-router-dom';
 
 function SavedCardsPage({ x, setData, }) {
   const [input, setInput] = useState("inti")
-  const [edit, setEdit] = useState(false);
-  const [editCardTitle, setEditCardTitle] = useState('new title');
+  const [editState, setEditState] = useState(false);
+  const [update, setUpdate] = useState('');
+  const [currentCard, setCurrentCard] = useState();
+
   const handleInput = (e) => {
     setInput(e.target.value);
 
   }
+  const handleUpdate = (e) => {
+    setUpdate(e.target.value);
+
+
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(input)
+
     create();
     setInput("");
   }
-  const handleEdit = () => {
-    console.log(0)
-  }
+
   const handle = (e) => {
-    if (e.target.innerHTML === "edit") {
-      console.log(e.target)
-      // edit
-      console.log("edit", e.target.value)
-      fetch(`http://localhost:5000/api/updatecard/${e.target.id}`, {
-        method: 'PATCH',
-        body: JSON.stringify({ title: editCardTitle }),
-        headers: {
-          "Content-Type": "application/json",
-        }
+    setEditState(!editState);
+    currentCard === e.target.id ? setCurrentCard(null) : null
+    if (e.target.innerHTML === "edit" && !editState) {
+      setCurrentCard(e.target.id);
+      setUpdate('')
+      // setEditState(!editState);
+
+      if (update === '') {
+        setUpdate(() => {
+
+          const title = x.filter((r) => {
+            return r._id === e.target.id
+          })
+          console.log('updatedTItle', title[0].title);
+
+          return title[0].title
+        });
 
 
-      }).then((res) => { res.json(); }).then((data) => {
-        console.log('JSONdata', data);
-      })
-    } else {
+      }
+
+      console.log("before IF", editState)
+
+
+
+    } else if (e.target.innerHTML === "submit" && editState) {
+      if (update !== '') {
+        console.log("IF", editState)
+        setData((prevState) => {
+          console.log('update', update);
+
+          const updatedData = prevState.map((r, i) => {
+            if (r._id === e.target.id) {
+              r.title = update;
+            }
+            return r;
+          })
+
+          console.log("updatedData", updatedData);
+          return updatedData
+        });
+
+        setUpdate('')
+
+        console.log("fetch", editState);
+        console.log("fetch", update);
+
+        fetch(`http://localhost:5000/api/updateCard/${e.target.id}`, {
+          method: "PATCH",
+          body: JSON.stringify({ title: update }),
+          headers: {
+            "Content-Type": "application/json",
+          }
+
+
+        })
+      }
+
+
+
+
+    }
+    else {
       // delete
-      fetch(`http://localhost:5000/api/deleteCard/${e.target.id}`, {
-        method: 'DELETE',
+      // fetch(`http://localhost:5000/api/deleteCard/${e.target.id}`, {
+      //   method: 'DELETE',
 
 
-      }).then((res) => { res.json(); }).then((data) => {
-        console.log('JSONdata', data); setData(x.filter((recipe) => { return recipe._id != e.target.id }))
-      });
+      // }).then((res) => { res.json(); }).then((data) => {
+      //   setData(x.filter((recipe) => { return recipe._id != e.target.id }))
+      // });
     }
 
   }
   const create = async () => {
-    console.log(input)
+
     if (input.length > 0) {
       await fetch(" http://localhost:5000/api/", {
         method: "POST",
@@ -54,12 +107,13 @@ function SavedCardsPage({ x, setData, }) {
         headers: {
           "Content-Type": "application/json",
         }
-      }).then(res => res.json()).then(updatedata => { console.log(updatedata); setData([...x, updatedata]) })
+      }).then(res => res.json()).then(updatedata => { ; setData([...x, updatedata]) })
     } else {
 
     }
 
   }
+
   return (
     <div className="h-screen  ">
       <form method="post" required>
@@ -69,10 +123,17 @@ function SavedCardsPage({ x, setData, }) {
 
       <div className='flex flex-wrap justify-between p-8  '>
         {x ? x.map((recipe, i) => {
-          const title = edit ? <input type="text"></input> : <span>{recipe.title}</span>
-          return (
 
-            <p key={i} className=" w-40 border flex justify-between">{title}<button id={recipe._id} className="bg-blue-700" type='submit' onClick={handle}>edit</button ><button id={recipe._id} className="bg-red-700" type='submit' onClick={handle}>X</button ></p>
+          return (
+            <div id={recipe._id} >
+              {<p>{recipe.title}</p>}
+              {currentCard === recipe._id && <input id={recipe._id} type="text" key={i} className=" w-40 border flex justify-between" value={
+                update
+              } onChange={handleUpdate} />}
+              <button id={recipe._id} className="bg-blue-700" type='submit' onClick={handle}>{currentCard === recipe._id ? "submit" : "edit"}</button ><button id={recipe._id} className="bg-red-700" type='submit' onClick={handle}>X</button >
+
+            </div>
+
           )
 
         }) : <p>nothing yet</p>}
